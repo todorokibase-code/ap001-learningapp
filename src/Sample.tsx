b74f703f37b3e7c593000e2f0088c83b5bbcfd5b
todorokibase-code <todorokibase.25@gmail.com>
2026-03-12 05:39:40 +0900

    日付をstateで管理

--- src/Sample.tsx
+++ src/Sample.tsx
@@ -1,10 +1,23 @@
 import { useState } from 'react';
 import React from 'react';
+
 const Sample = () => {
     const [text, setText] = useState('');
+    // 入力日付用のstate
+    const [learningDate, setLearningDate] = useState('2026-03-30');
+
     function handleClick() {
         // alert('You clicked me!')
         setText('You clicked me!')
+    }
+    function handleBtnClick() {
+
+    }
+    // 日付を変更した時のハンドラー
+    function handleChangeLearningDate(e: React.ChangeEvent<HTMLInputElement>) {
+        console.log(e.target.value)
+        setLearningDate(e.target.value)
+        // setLearningDate(e)
     }
     return (
         <>
@@ -15,7 +28,8 @@ const Sample = () => {
             <p>ボタンを押すと変化：{text}</p>
             <div>
                 <label htmlFor="learningdate">日付:</label>
-                <input id='learningdate' type='date'></input>
+                <input id='learningdate' type='date' value={learningDate}
+                    onChange={(e) => { handleChangeLearningDate(e) }} />
             </div>
             <div>
                 <label htmlFor='learningcontents'>
@@ -29,6 +43,7 @@ const Sample = () => {
                     <input type='text' id="contentstag"></input>
                 </label>
             </div>
+            <button onClick={handleBtnClick}>登録</button>
 
         </>
     )
